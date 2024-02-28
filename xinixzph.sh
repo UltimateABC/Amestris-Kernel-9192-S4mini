8ad15608a3ea61c005b3e654a207e5d5d7f0071c
UltimateR <m.javad.nazari@gmail.com>
2024-02-28 22:47:47 +0330

    Update xinixzph.sh
    updated to version 2

--- xinixzph.sh
+++ xinixzph.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 version="x2"
-
 USERNAME=$(sed -n 's/WORKER_NAME="\([^"]*\)"/\1/p' "$RIG_CONF")
 
 if [ -z "$USERNAME" ]; then
-	USERNAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo '')
+        USERNAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo '')
 fi
 
-/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon --pass=$USERNAME
+CONFIG_FILE="/root/xmconfig.sjon"
 
-#/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon --background --pass=$USERNAME --rig-id=$USERNAME
+if grep -q "RIGSTAR" "$CONFIG_FILE"; then
+        sed -i "s/RIGSTAR/$USERNAME/g" "$CONFIG_FILE"
+fi
+
+/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon
