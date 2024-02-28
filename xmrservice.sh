43119ee724f11513db838cd382f4d16c4d625086
UltimateR <m.javad.nazari@gmail.com>
2024-02-28 12:26:14 +0330

    Update xmrservice.sh
    first release v2.3

--- xmrservice.sh
+++ xmrservice.sh
@@ -1,80 +1,38 @@
-#!/bin/sh
-echo "XM Script Version 2.2 , Wait 5 Seconds..."
+#!/bin/bash
+
+echo "XM Script Version 2.3"
 
 cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 CPUTEMP=$((cpu_temp/1000))
 
-FREERM=$(free -h | grep 'Mem' | awk '{print $7}')
-
-
-if [[ $FREERM == *M ]]; then
-	FREERM=${FREERM//M/}
-	FREERM=$(echo $FREERM | xargs)
-fi
-
-if [[ $FREERM == *G ]]; then
-	FREERM=${FREERM//G/}
-	FREERM=$(echo $FREERM | xargs)
-	FREERM=$(echo "$FREERM * 1000" | bc)
-fi
+FREERM=$(free -m | awk '/^Mem/{print $7}')
 
 kill_xmrig() {
-	#echo "Killing xmrig..."
-	killall -9 xmrig
-	#echo "Miner has been stopped."
+    killall -9 xmrig
 }
 
 start_xmrig() {
-	#echo "Starting xmrig..."
-	bash /root/xinixzph.sh </dev/null &>/dev/null &
+    bash /root/xinixzph.sh </dev/null &>/dev/null &
 }
 
 xrig=$(pgrep -cx xmrig)
 
-#echo "XRIG Running Proxy=$xrig"
-
-CPUTEMP=$(get_cpu_temp)
-
-#echo "CPU Temprature =$CPUTEMP"
-#echo "Available Ram  =$FREERM"
-#echo
-	
 if [ "$xrig" -eq 1 ]; then
-	#echo "Miner is currently running..."
-	
-	if [ "$CPUTEMP" -gt 75 ]; then
-		#echo "   - High CPU Temp, Exiting..."
-		kill_xmrig
-	fi
-
-	if (( $(echo "$FREERM < 300" | bc -l) )); then
-		#echo "   - Not enough RAM, Exiting..."
-		kill_xmrig
-	fi
-	
+    if [ "$CPUTEMP" -gt 75 ] || [ "$FREERM" -lt 300 ]; then
+        kill_xmrig
+    fi
 elif [ "$xrig" -gt 1 ]; then
-	#echo "Miner is running more than usual..."
-	kill_xmrig
-	sleep 2
-	start_xmrig
-
+    kill_xmrig
+    sleep 2
+    start_xmrig
 elif [ "$xrig" -eq 0 ]; then
-	echo "Miner is not running..."
-	echo "Temp= $CPUTEMP"
-	echo "FREERM= $FREERM"
+    echo "Miner is not running..."
+    echo "Temp= $CPUTEMP°C"
+    echo "FREERM= $FREERM MB"
 
-	if [ "$CPUTEMP" -lt 60 ]; then
-		echo "CPU Temprature is OK : $CPUTEMP"
-		
-			if [ "$FREERM" -gt 2500 ]; then
-			echo "Available Ram is OK : $FREERM"        
-			start_xmrig
-		else
-			echo "Available Ram is not OK : $FREERM"
-			echo "Conditions not met to start the miner."
-		fi
-	else
-        	echo "CPU Temprature is not OK : $CPUTEMP"
-		echo "Conditions not met to start the miner."
-	fi
+    if [ "$CPUTEMP" -lt 60 ] && [ "$FREERM" -gt 2500 ]; then
+        start_xmrig
+    else
+        echo "Conditions not met to start the miner."
+    fi
 fi
