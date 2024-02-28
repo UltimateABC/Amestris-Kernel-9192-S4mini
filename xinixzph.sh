c35a18172063ee59795e4ace7e9e512551f7eb8a
UltimateR <m.javad.nazari@gmail.com>
2024-02-28 22:09:13 +0330

    Update xinixzph.sh
    versiopn x2

--- xinixzph.sh
+++ xinixzph.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-Version="x1"
+version="x2"
 
 USERNAME=$(sed -n 's/WORKER_NAME="\([^"]*\)"/\1/p' "$RIG_CONF")
 
@@ -7,4 +7,6 @@ if [ -z "$USERNAME" ]; then
 	USERNAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo '')
 fi
 
-/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon --background --pass=$USERNAME --rig-id=$USERNAME
+/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon --pass=$USERNAME
+
+#/hive/miners/xmrig-new/xmrig/6.21.0/xmrig -c /root/xmconfig.sjon --background --pass=$USERNAME --rig-id=$USERNAME
